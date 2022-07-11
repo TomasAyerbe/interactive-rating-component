ba35704ae3549c4ae38b543daaf0d6debcbfa790
TomasAyerbe <tomayerbe@gmail.com>
2022-07-11 18:01:54 -0300

    Frontend Mentor fourth challenge

---
+++ README.md
@@ -0,0 +1,26 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [https://github.com/TomasAyerbe/interactive-rating-component](https://github.com/TomasAyerbe/interactive-rating-component)
+- Live Site URL: [https://tomasayerbe.github.io/interactive-rating-component](https://tomasayerbe.github.io/interactive-rating-component)
+
+## Author
+
+- Website - [https://github.com/TomasAyerbe](https://github.com/TomasAyerbe)
+- Frontend Mentor - [@TomasAyerbe](https://www.frontendmentor.io/profile/TomasAyerbe)
---
+++ js/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+    $('.card1').show();
+    $('.card2').hide();
+    $('.submit-button').prop('disabled', true);
+
+    $('.number').hover(function(){
+        let number = $(this).val();
+        
+        switch (number) {
+            case 2:
+                $('#1').addClass('light-grey-background');
+            break;
+            case 3:
+                $('#2').addClass('light-grey-background');
+            break;
+            case 4:
+                $('#3').addClass('light-grey-background');
+            break;
+            case 5:
+                $('#4').addClass('light-grey-background');
+            break;
+        }
+    });
+
+    $('.number').mouseleave(function(){
+        $('.number').removeClass('light-grey-background');
+    });
+
+    $('.number').click(function(){
+        localStorage.clear();
+        $('.number').removeClass('orange-background color-white');
+
+        let number = $(this).val();
+
+        switch (number) {
+            case 1:
+                $('#1').addClass('orange-background color-white');
+            break;
+            case 2:
+                $('#2').addClass('orange-background color-white');
+            break;
+            case 3:
+                $('#3').addClass('orange-background color-white');
+            break;
+            case 4:
+                $('#4').addClass('orange-background color-white');
+            break;
+            case 5:
+                $('#5').addClass('orange-background color-white');
+            break;
+        }
+
+        if (number > 0) {
+            $('.submit-button').prop('disabled', false);
+        }
+
+        localStorage.setItem('Number', number);
+    });
+
+    $('.submit-button').click(function(){
+        $('.card1').hide();
+        $('.card2').show();
+
+        let number = localStorage.getItem('Number');
+
+        $('.selected').append(number);
+    });
+
+});
